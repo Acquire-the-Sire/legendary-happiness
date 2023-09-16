171128df3dd0ecb66cbdb18afc4f52521d8d048a
nalam <nalambert03@gmail.com>
2023-09-16 11:07:51 -0500

    start

---
+++ mysql_connection.py
@@ -0,0 +1,49 @@
+import mysql.connector
+
+from mysql.connector import Error
+
+
+
+try:
+
+    connection = mysql.connector.connect(
+
+        host = "cis3368fall.cormgjdgnb98.us-east-1.rds.amazonaws.com", # this is your db endpoint address
+
+        user = "admin", # username for MySQL db on AWS
+
+        password = "2oranges4me", # password for MySQL db on AWS
+
+        database = "cis3368falldb" # database name which we created in MySQL workbench.
+
+    )
+
+except Error as err:
+
+    print("failure in establishing connection")
+
+    exit()
+
+
+
+print("connection successful")
+
+
+
+cursor = connection.cursor(dictionary=True)
+
+sql = "select * from Users"
+
+cursor.execute(sql)
+
+
+
+results = cursor.fetchall()
+
+
+
+for datarow in results:
+
+    print(f"firstname: {datarow['firstname']}, lastname: {datarow['lastname']}, email: {datarow['email']}")
+
+
