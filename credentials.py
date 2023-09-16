7d102ee765b1fe6276c14d6cea2fd008a71dd473
nalam <nalambert03@gmail.com>
2023-09-16 11:14:38 -0500

    start

---
+++ credentials.py
@@ -0,0 +1,8 @@
+class Creds:
+    conString = "cis3368fall.cormgjdgnb98.us-east-1.rds.amazonaws.com", # this is your db endpoint address
+
+    userName = "admin", # username for MySQL db on AWS
+
+    password = "2oranges4me", # password for MySQL db on AWS
+
+    dbName = "cis3368falldb" # database name which we created in MySQL workbench.
